ed9b5f22efa2871eb3ab4c49a2ac394add230be6
unknown <vredes@live.com>
2020-05-22 00:09:10 -0400

    Componente de cartas v0.1 funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CARDS } from '../db-data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cardsProject';
+ 
+  card1 = CARDS[0];
+  card2 = CARDS[1];
+
 }
---
+++ src/app/model/card.ts
@@ -0,0 +1,9 @@
+
+export interface Card {
+    id:number;
+    description:string;
+    iconUrl: string;
+    longDescription: string;
+    category:string;
+    lessonsCount:number;
+}
